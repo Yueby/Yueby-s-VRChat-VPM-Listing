e380d5b1db38dfa5acc6b4cbfe61260bb8a2a144
月白 <2249351762@qq.com>
2023-11-19 23:23:04 +0800

    Update SyncBoneTransform.cs

--- Editor/Scripts/Other/SyncBoneTransform.cs
+++ Editor/Scripts/Other/SyncBoneTransform.cs
@@ -7,6 +7,7 @@ namespace Yueby.AvatarTools.Other
     public class SyncBoneTransform : EditorWindow
     {
         private Transform _origin, _target;
+        private bool _isUsingWorldSpace;
 
         private void OnGUI()
         {
@@ -16,13 +17,16 @@ namespace Yueby.AvatarTools.Other
             {
                 _origin = (Transform)YuebyUtil.ObjectField("源", 50, _origin, typeof(Transform), true);
                 _target = (Transform)YuebyUtil.ObjectField("当前", 50, _target, typeof(Transform), true);
+                _isUsingWorldSpace = YuebyUtil.Radio(_isUsingWorldSpace, "使用世界坐标");
             });
 
             YuebyUtil.VerticalEGLTitled("操作", () =>
             {
                 if (GUILayout.Button("同步"))
+                {
                     if (_origin != null && _target != null)
                         SyncBone(_origin, _target);
+                }
             });
         }
 
@@ -32,18 +36,37 @@ namespace Yueby.AvatarTools.Other
             var window = GetWindow<SyncBoneTransform>();
             window.titleContent = new GUIContent("同步骨骼Transform");
             window.minSize = new Vector2(400, 600);
-        } // ReSharper disable Unity.PerformanceAnalysis
+        }
+
+        // ReSharper disable Unity.PerformanceAnalysis
         private void SyncBone(Transform origin, Transform target)
         {
+            if (_isUsingWorldSpace)
+            {
+                origin.transform.position = Vector3.zero;
+                target.transform.position = Vector3.zero;
+            }
+
             foreach (var originTrans in origin.GetComponentsInChildren<Transform>(true))
             foreach (var targetTrans in target.GetComponentsInChildren<Transform>())
-                if (targetTrans.name.Equals(originTrans.name))
+            {
+                if (!targetTrans.name.Equals(originTrans.name)) continue;
+
+                if (!_isUsingWorldSpace)
                 {
                     targetTrans.localPosition = originTrans.localPosition;
                     targetTrans.localRotation = originTrans.localRotation;
                     targetTrans.localScale = originTrans.localScale;
-                    break;
                 }
+                else
+                {
+                    targetTrans.position = originTrans.position;
+                    targetTrans.rotation = originTrans.rotation;
+                    targetTrans.localScale = originTrans.localScale;
+                }
+
+                break;
+            }
         }
     }
 }
