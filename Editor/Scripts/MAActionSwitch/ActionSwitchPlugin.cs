91b00afe462e01d8bebc0cc47470ed992260c25c
月白 <yueby_sp@qq.com>
2024-06-03 04:06:56 +0800

    update some bugs

--- Editor/Scripts/MAActionSwitch/ActionSwitchPlugin.cs
+++ Editor/Scripts/MAActionSwitch/ActionSwitchPlugin.cs
@@ -35,7 +35,7 @@ namespace Yueby.AvatarTools.MAActionSwitch
 
     class ActionSwitchBuilder
     {
-
+        private static string ToolLabel = "MA Action Switch/";
         private static string AssetsPath => "Packages/com.yueby.avatartools/Editor/Assets/MAActionSwitch";
         private static string EmptyClipPath => AssetsPath + "/Animations/Empty.anim";
 
@@ -58,40 +58,58 @@ namespace Yueby.AvatarTools.MAActionSwitch
         {
             var transitionDuration = 0.1f;
             var writeDefault = true;
-            var parameterName = actionSwitch.Name;
+            var parameterName = ToolLabel + actionSwitch.Name;
+            var emptyClip = new AnimationClip();
             var animatorController = AnimatorController.CreateAnimatorControllerAtPath($"{AssetsPath}/Generated/{actionSwitch.name}.controller");
+
             animatorController.AddParameter(name: parameterName, AnimatorControllerParameterType.Int);
 
             var layer = new AnimatorControllerLayer
             {
-                name = actionSwitch.Name,
+                name = parameterName,
                 defaultWeight = 1,
                 stateMachine = new AnimatorStateMachine
                 {
-                    name = actionSwitch.Name
+                    name = parameterName
                 }
             };
 
-            var defaultState = layer.stateMachine.AddState("Default");
-            var defaultStateTransition = layer.stateMachine.AddAnyStateTransition(defaultState);
-            defaultStateTransition.AddCondition(AnimatorConditionMode.Equals, 0, parameterName);
-            defaultState.writeDefaultValues = writeDefault;
-            defaultState.motion = AssetDatabase.LoadAssetAtPath<AnimationClip>(EmptyClipPath);
-            defaultStateTransition.duration = transitionDuration;
-            AddTrackingControlStateBehaviour(defaultState, VRC.SDKBase.VRC_AnimatorTrackingControl.TrackingType.Tracking);
+            var waitState = CreateAnimatorState(layer.stateMachine, "Wait", emptyClip, writeDefault);
+            var readyState = CreateAnimatorState(layer.stateMachine, "Ready", emptyClip, writeDefault);
+            var endState = CreateAnimatorState(layer.stateMachine, "End", emptyClip, writeDefault);
+
+            AddTrackingControlStateBehaviour(readyState, VRC.SDKBase.VRC_AnimatorTrackingControl.TrackingType.Animation);
+            AddTrackingControlStateBehaviour(endState, VRC.SDKBase.VRC_AnimatorTrackingControl.TrackingType.Tracking);
+            AddPlayableLayerControl(readyState, 1);
+            AddPlayableLayerControl(endState, 0);
+
+            // var driver = endState.AddStateMachineBehaviour<VRCAvatarParameterDriver>();
+            // driver.parameters.Add(new VRC.SDKBase.VRC_AvatarParameterDriver.Parameter
+            // {
+            //     name = parameterName,
+            //     value = 0,
+            //     type = VRC.SDKBase.VRC_AvatarParameterDriver.ChangeType.Set
+            // });
+
+            layer.stateMachine.defaultState = waitState;
+            AddIntTransition(waitState, readyState, AnimatorConditionMode.NotEqual, parameterName, 0, transitionDuration);
+            var exitTransition = endState.AddExitTransition();
+            exitTransition.hasExitTime = true;
+            exitTransition.exitTime = 0;
+            exitTransition.duration = transitionDuration;
+
             animatorController.AddLayer(layer);
             for (int i = 0; i < actionSwitch.Actions.Count; i++)
             {
                 ActionElement actionElement = actionSwitch.Actions[i];
-                var state = layer.stateMachine.AddState(actionElement.Name);
-                state.motion = actionElement.Clip;
-                state.writeDefaultValues = writeDefault;
-                AddTrackingControlStateBehaviour(state, VRC.SDKBase.VRC_AnimatorTrackingControl.TrackingType.Animation);
+                var actionState = CreateAnimatorState(layer.stateMachine, actionElement.Name, actionElement.Clip, writeDefault);
 
+                AddIntTransition(readyState, actionState, AnimatorConditionMode.Equals, parameterName, i + 1, transitionDuration);
+                AddIntTransition(actionState, endState, AnimatorConditionMode.NotEqual, parameterName, i + 1, transitionDuration);
                 // add anyState transition
-                var transition = layer.stateMachine.AddAnyStateTransition(state);
-                transition.AddCondition(AnimatorConditionMode.Equals, i + 1, parameterName);
-                transition.duration = transitionDuration;
+                // var transition = layer.stateMachine.AddAnyStateTransition(state);
+                // transition.AddCondition(AnimatorConditionMode.Equals, i + 1, parameterName);
+                // transition.duration = transitionDuration;
             }
 
             animatorController.RemoveLayer(0);
@@ -100,20 +118,47 @@ namespace Yueby.AvatarTools.MAActionSwitch
 
             return animatorController;
 
-            void AddTrackingControlStateBehaviour(AnimatorState state, VRC.SDKBase.VRC_AnimatorTrackingControl.TrackingType type)
-            {
-                var control = state.AddStateMachineBehaviour<VRCAnimatorTrackingControl>();
-                control.trackingEyes = type;
-                control.trackingHead = type;
-                control.trackingLeftHand = type;
-                control.trackingRightHand = type;
-                control.trackingMouth = type;
-                control.trackingHip = type;
-                control.trackingLeftFingers = type;
-                control.trackingRightFingers = type;
-                control.trackingLeftFoot = type;
-                control.trackingRightFoot = type;
-            }
+        }
+
+        private static void AddTrackingControlStateBehaviour(AnimatorState state, VRC.SDKBase.VRC_AnimatorTrackingControl.TrackingType type)
+        {
+            var control = state.AddStateMachineBehaviour<VRCAnimatorTrackingControl>();
+            control.trackingEyes = type;
+            control.trackingHead = type;
+            control.trackingLeftHand = type;
+            control.trackingRightHand = type;
+            control.trackingMouth = type;
+            control.trackingHip = type;
+            control.trackingLeftFingers = type;
+            control.trackingRightFingers = type;
+            control.trackingLeftFoot = type;
+            control.trackingRightFoot = type;
+        }
+
+        private static AnimatorState CreateAnimatorState(AnimatorStateMachine stateMachine, string name, AnimationClip clip, bool writeDefault)
+        {
+            var state = stateMachine.AddState(name);
+            state.motion = clip;
+            state.writeDefaultValues = writeDefault;
+            return state;
+        }
+
+        private static AnimatorState AddPlayableLayerControl(AnimatorState state, float weight)
+        {
+            var playable = state.AddStateMachineBehaviour<VRCPlayableLayerControl>();
+            playable.layer = VRC.SDKBase.VRC_PlayableLayerControl.BlendableLayer.Action;
+            playable.goalWeight = weight;
+            return state;
+        }
+
+        private static AnimatorState AddIntTransition(AnimatorState state, AnimatorState destination, AnimatorConditionMode mode, string parameterName, int value, float duration, bool hasExitTime = false)
+        {
+            var transition = state.AddTransition(destination);
+            transition.AddCondition(mode, value, parameterName);
+            transition.duration = duration;
+            transition.hasExitTime = hasExitTime;
+            transition.hasFixedDuration = true;
+            return destination;
         }
 
         public static void GeneratePath()
@@ -127,7 +172,7 @@ namespace Yueby.AvatarTools.MAActionSwitch
 
         private static void MakeMAComponents(GameObject avatarRootObject, RuntimeAnimatorController animator, ActionSwitch actionSwitch)
         {
-            var parameterName = actionSwitch.Name;
+            var parameterName = ToolLabel + actionSwitch.Name;
             var maParam = avatarRootObject.GetComponent<ModularAvatarParameters>() ?? avatarRootObject.AddComponent<ModularAvatarParameters>();
             maParam.parameters.Add(new ParameterConfig
             {
@@ -142,7 +187,7 @@ namespace Yueby.AvatarTools.MAActionSwitch
             });
 
             var maMenuItem = actionSwitch.GetComponent<ModularAvatarMenuItem>() ?? actionSwitch.gameObject.AddComponent<ModularAvatarMenuItem>();
-            maMenuItem.Control.name = actionSwitch.Name;
+            maMenuItem.Control.name = parameterName;
 
             CreateChild("None", 0, maMenuItem.transform);
 
@@ -154,7 +199,7 @@ namespace Yueby.AvatarTools.MAActionSwitch
 
             var mergeAnimator = actionSwitch.GetComponent<ModularAvatarMergeAnimator>() ?? actionSwitch.gameObject.AddComponent<ModularAvatarMergeAnimator>();
             mergeAnimator.animator = animator;
-            mergeAnimator.layerType = VRC.SDK3.Avatars.Components.VRCAvatarDescriptor.AnimLayerType.Base;
+            mergeAnimator.layerType = VRC.SDK3.Avatars.Components.VRCAvatarDescriptor.AnimLayerType.Action;
             mergeAnimator.pathMode = MergeAnimatorPathMode.Absolute;
             mergeAnimator.matchAvatarWriteDefaults = true;
 
